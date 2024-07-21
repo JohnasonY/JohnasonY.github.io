81171dc42e2d0ed2ae3939a1909a723e79c8c3d1
JohnasonY <157220107+JohnasonY@users.noreply.github.com>
2024-07-20 23:44:14 -0400

    Update AmiiboDetails.js

--- FinalProject/AmiiboDetails.js
+++ FinalProject/AmiiboDetails.js
@@ -1,4 +1,4 @@
-async function AmiiboDetails(amiiboID){
+async function AmiiboDetails(amiiboID, amiiboCharacter, amiiboGameSeries){
 
     let AmiiboURL = `https://www.amiiboapi.com/api/amiibo/?id=${amiiboID}`;
 
@@ -32,7 +32,150 @@ async function AmiiboDetails(amiiboID){
                     North America: ${Amiibo.release.na}<br>
                 </p>
             </div>
-        </div>
+        </div>    
     `;
 
+    RelatedCharacter(amiiboID, amiiboCharacter);
+    RelatedGameSeries(amiiboID, amiiboGameSeries);
 }
+
+async function RelatedCharacter(amiiboID, amiiboCharacter, amiiboGameSeries){
+    let CharacterURL = `https://www.amiiboapi.com/api/amiibo/?character=${amiiboCharacter}`;
+
+    const options1 = {
+        method: 'GET',
+    };
+
+    const response1 = await fetch(CharacterURL, options1);
+    const result1 = await response1.text();
+
+    const wholeObject1 = JSON.parse(result1);
+
+    const amiiboArray = wholeObject1.amiibo;
+
+    let relatedCharacter = document.getElementById("relatedCharacter");
+
+    if(amiiboArray.length-1 > 0){
+        relatedCharacter.innerHTML = `<br>
+        <h2>Related Character</h2>
+        <br>
+    `;
+    }
+    else{
+        relatedCharacter.innerHTML = "";
+    }
+
+    let newRow;
+
+
+    let count = 0;
+
+    for (let i = 0; i < amiiboArray.length; i++) {
+
+
+        let amiiboCurrentID = amiiboArray[i].head + amiiboArray[i].tail;
+
+        if (amiiboCurrentID === amiiboID)
+            continue;
+
+        if (count % 4 === 0) {
+            newRow = document.createElement("div");
+            newRow.className = "row";
+            relatedCharacter.appendChild(newRow);
+
+            newBreakLine = document.createElement("br");
+            relatedCharacter.appendChild(newBreakLine);
+        }
+
+        let amiiboImage = amiiboArray[i].image;
+        let amiiboName = amiiboArray[i].name;
+        let amiiboCharacter = amiiboArray[i].character;
+        let amiiboGameSeries = amiiboArray[i].gameSeries;
+
+        newRow.insertAdjacentHTML("beforeend", `
+                        <div class="col-3">
+                            <a href="#" onclick="AmiiboDetails('${amiiboCurrentID}', '${amiiboCharacter}', '${amiiboGameSeries}')">
+                                <div class="img-thumbnail text-center h-100" style="background-color: beige">
+                                    <img src="${amiiboImage}" alt="${amiiboName}" height="200px" ">
+                                    <hr>
+                                    <h3>${amiiboName}</h3>
+                                </div>
+                            </a>
+                        </div>
+                    `);
+
+
+        count++;
+    }
+}
+
+async function RelatedGameSeries(amiiboID, amiiboGameSeries){
+    let GameSeriesURL = `https://www.amiiboapi.com/api/amiibo/?gameseries=${amiiboGameSeries}`;
+
+    const options1 = {
+        method: 'GET',
+    };
+
+    const response1 = await fetch(GameSeriesURL, options1);
+    const result1 = await response1.text();
+
+    const wholeObject1 = JSON.parse(result1);
+
+    const amiiboArray = wholeObject1.amiibo;
+
+    let relatedGame = document.getElementById("relatedGame");
+
+    if(amiiboArray.length-1 > 0){
+        relatedGame.innerHTML = `<br>
+        <h2>Related GameSeries</h2>
+        <br>
+    `;
+    }
+    else{
+        relatedGame.innerHTML = "";
+    }
+
+    let newRow;
+
+
+    let count = 0;
+
+    for (let i = 0; i < amiiboArray.length; i++) {
+
+
+        let amiiboCurrentID = amiiboArray[i].head + amiiboArray[i].tail;
+
+        if (amiiboCurrentID === amiiboID)
+            continue;
+
+        if (count % 4 === 0) {
+            newRow = document.createElement("div");
+            newRow.className = "row";
+            relatedGame.appendChild(newRow);
+
+            newBreakLine = document.createElement("br");
+            relatedGame.appendChild(newBreakLine);
+        }
+
+        let amiiboImage = amiiboArray[i].image;
+        let amiiboName = amiiboArray[i].name;
+        let amiiboCharacter = amiiboArray[i].character;
+        let amiiboGameSeries = amiiboArray[i].gameSeries;
+
+        newRow.insertAdjacentHTML("beforeend", `
+                        <div class="col-3">
+                            <a href="#" onclick="AmiiboDetails('${amiiboCurrentID}', '${amiiboCharacter}', '${amiiboGameSeries}')">
+                                <div class="img-thumbnail text-center h-100" style="background-color: beige">
+                                    <img src="${amiiboImage}" alt="${amiiboName}" height="200px" ">
+                                    <hr>
+                                    <h3>${amiiboName}</h3>
+                                </div>
+                            </a>
+                        </div>
+                    `);
+
+
+        count++;
+    }
+}
+
